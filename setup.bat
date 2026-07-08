765c821501e447b766b07a654628c05faf5f0751
NeedNotApply <mrnickvoice@gmail.com>
2026-07-08 14:02:40 -0700

    Fix Windows setup run instructions and dependency install

--- setup.bat
+++ setup.bat
@@ -25,10 +25,11 @@ if not exist "%TOOLS_DIR%" mkdir "%TOOLS_DIR%"
 python -m pip install --upgrade pip
 python -m pip install --user pipx
 python -m pipx ensurepath
-set "PATH=%USERPROFILE%\AppData\Roaming\Python\Python311\Scripts;%USERPROFILE%\.local\bin;%PATH%"
+for /f "delims=" %%P in ('python -c "import site; print(site.USER_BASE + r'\Scripts')"') do set "USER_PY_SCRIPTS=%%P"
+set "PATH=%USER_PY_SCRIPTS%;%USERPROFILE%\.local\bin;%PATH%"
 
 echo.
-echo [1/6] Setting up Sherlock...
+echo [1/9] Setting up Sherlock...
 cd /d "%TOOLS_DIR%"
 if not exist sherlock mkdir sherlock
 cd /d "%TOOLS_DIR%\sherlock"
@@ -39,46 +40,48 @@ python -m pip install sherlock-project
 call deactivate
 
 echo.
-echo [2/6] Cloning and setting up cupidcr4wl...
+echo [2/9] Cloning and setting up cupidcr4wl...
 cd /d "%TOOLS_DIR%"
 if not exist cupidcr4wl git clone https://github.com/OSINTI4L/cupidcr4wl
 cd /d "%TOOLS_DIR%\cupidcr4wl"
 python -m venv cupidcr4wlvenv
 call cupidcr4wlvenv\Scripts\activate.bat
+python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 call deactivate
 
 echo.
-echo [3/6] Cloning and setting up blackbird...
+echo [3/9] Cloning and setting up blackbird...
 cd /d "%TOOLS_DIR%"
 if not exist blackbird git clone https://github.com/p1ngul1n0/blackbird
 cd /d "%TOOLS_DIR%\blackbird"
 python -m venv blackbirdvenv
 call blackbirdvenv\Scripts\activate.bat
+python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 call deactivate
 
 echo.
-echo [4/6] Setting up holehe...
+echo [4/9] Setting up holehe...
 cd /d "%TOOLS_DIR%"
 if not exist holehe mkdir holehe
 cd /d "%TOOLS_DIR%\holehe"
 python -m venv holehevenv
 call holehevenv\Scripts\activate.bat
+python -m pip install --upgrade pip
 python -m pip install holehe
 call deactivate
 
 echo.
-echo [5/6] Setting up user-scanner...
+echo [5/9] Setting up user-scanner...
 cd /d "%TOOLS_DIR%"
 if not exist user-scanner git clone https://github.com/mishakorzik/UserFinder user-scanner
 cd /d "%TOOLS_DIR%\user-scanner"
 python -m venv userscannervenv
 call userscannervenv\Scripts\activate.bat
+python -m pip install --upgrade pip
 python -m pip install user-scanner
 call deactivate
-
-
 
 echo.
 echo [6/9] Setting up whois...
@@ -87,6 +90,7 @@ if not exist whois mkdir whois
 cd /d "%TOOLS_DIR%\whois"
 python -m venv whoisvenv
 call whoisvenv\Scripts\activate.bat
+python -m pip install --upgrade pip
 python -m pip install python-whois
 call deactivate
 
@@ -97,6 +101,7 @@ if not exist theHarvester mkdir theHarvester
 cd /d "%TOOLS_DIR%\theHarvester"
 python -m venv theharvestervenv
 call theharvestervenv\Scripts\activate.bat
+python -m pip install --upgrade pip
 python -m pip install theHarvester
 call deactivate
 
@@ -107,6 +112,7 @@ if not exist sublist3r mkdir sublist3r
 cd /d "%TOOLS_DIR%\sublist3r"
 python -m venv sublist3rvenv
 call sublist3rvenv\Scripts\activate.bat
+python -m pip install --upgrade pip
 python -m pip install sublist3r
 call deactivate
 
@@ -115,14 +121,17 @@ echo [9/9] Setting up bot virtual environment...
 cd /d "%BOT_DIR%"
 python -m venv discordbotvenv
 call discordbotvenv\Scripts\activate.bat
-python -m pip install discord.py requests
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 call deactivate
 
 echo.
 echo ================================================
 echo Setup complete.
-echo Next: copy bot.py into %BOT_DIR% and configure config.json values.
+echo Next: configure config.json with BOT_TOKEN.
 echo Run manually with:
 echo   cd /d %BOT_DIR%
-echo   discordbotvenv\Scripts\python osint_bot.py
+echo   run_bot.bat
+echo or:
+echo   discordbotvenv\Scripts\python.exe bot.py
 echo ================================================
