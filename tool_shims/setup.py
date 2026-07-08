3c2474159e8124f6a73218016ba29dc727fc9de8
NeedNotApply <mrnickvoice@gmail.com>
2026-07-08 16:24:25 -0700

    Depend on requests for tool shims

--- tool_shims/setup.py
+++ tool_shims/setup.py
@@ -2,8 +2,12 @@ from setuptools import setup
 
 setup(
     name='osintbot-tool-shims',
-    version='0.1.0',
+    version='0.2.0',
     py_modules=['osintbot_tool_shims'],
+    install_requires=[
+        'requests>=2.32',
+        'certifi>=2024.8.30',
+    ],
     entry_points={
         'console_scripts': [
             'sherlock=osintbot_tool_shims:sherlock_main',
