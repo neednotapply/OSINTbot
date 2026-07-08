640e507f96c81ab168ee1928bc0fec0d8a82b00f
NeedNotApply <mrnickvoice@gmail.com>
2026-07-08 15:51:06 -0700

    Install SSL patch during Windows setup

--- setup.bat
+++ setup.bat
@@ -29,18 +29,18 @@ for /f "delims=" %%P in ('python -c "import site; print(site.USER_BASE + r'\Scri
 set "PATH=%USER_PY_SCRIPTS%;%USERPROFILE%\.local\bin;%PATH%"
 
 echo.
-echo [1/9] Setting up Sherlock...
+echo [1/10] Setting up Sherlock...
 cd /d "%TOOLS_DIR%"
 if not exist sherlock mkdir sherlock
 cd /d "%TOOLS_DIR%\sherlock"
 python -m venv sherlockvenv
 call sherlockvenv\Scripts\activate.bat
 python -m pip install --upgrade pip
-python -m pip install sherlock-project
+python -m pip install sherlock-project certifi
 call deactivate
 
 echo.
-echo [2/9] Cloning and setting up cupidcr4wl...
+echo [2/10] Cloning and setting up cupidcr4wl...
 cd /d "%TOOLS_DIR%"
 if not exist cupidcr4wl git clone https://github.com/OSINTI4L/cupidcr4wl
 cd /d "%TOOLS_DIR%\cupidcr4wl"
@@ -48,10 +48,11 @@ python -m venv cupidcr4wlvenv
 call cupidcr4wlvenv\Scripts\activate.bat
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
+python -m pip install --upgrade certifi
 call deactivate
 
 echo.
-echo [3/9] Cloning and setting up blackbird...
+echo [3/10] Cloning and setting up blackbird...
 cd /d "%TOOLS_DIR%"
 if not exist blackbird git clone https://github.com/p1ngul1n0/blackbird
 cd /d "%TOOLS_DIR%\blackbird"
@@ -59,71 +60,76 @@ python -m venv blackbirdvenv
 call blackbirdvenv\Scripts\activate.bat
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
+python -m pip install --upgrade certifi
 call deactivate
 
 echo.
-echo [4/9] Setting up holehe...
+echo [4/10] Setting up holehe...
 cd /d "%TOOLS_DIR%"
 if not exist holehe mkdir holehe
 cd /d "%TOOLS_DIR%\holehe"
 python -m venv holehevenv
 call holehevenv\Scripts\activate.bat
 python -m pip install --upgrade pip
-python -m pip install holehe
+python -m pip install holehe certifi
 call deactivate
 
 echo.
-echo [5/9] Setting up user-scanner...
+echo [5/10] Setting up user-scanner...
 cd /d "%TOOLS_DIR%"
 if not exist user-scanner git clone https://github.com/mishakorzik/UserFinder user-scanner
 cd /d "%TOOLS_DIR%\user-scanner"
 python -m venv userscannervenv
 call userscannervenv\Scripts\activate.bat
 python -m pip install --upgrade pip
-python -m pip install user-scanner
+python -m pip install user-scanner certifi
 call deactivate
 
 echo.
-echo [6/9] Setting up whois...
+echo [6/10] Setting up whois...
 cd /d "%TOOLS_DIR%"
 if not exist whois mkdir whois
 cd /d "%TOOLS_DIR%\whois"
 python -m venv whoisvenv
 call whoisvenv\Scripts\activate.bat
 python -m pip install --upgrade pip
-python -m pip install python-whois
+python -m pip install python-whois certifi
 call deactivate
 
 echo.
-echo [7/9] Setting up theHarvester...
+echo [7/10] Setting up theHarvester...
 cd /d "%TOOLS_DIR%"
 if not exist theHarvester mkdir theHarvester
 cd /d "%TOOLS_DIR%\theHarvester"
 python -m venv theharvestervenv
 call theharvestervenv\Scripts\activate.bat
 python -m pip install --upgrade pip
-python -m pip install theHarvester
+python -m pip install theHarvester certifi
 call deactivate
 
 echo.
-echo [8/9] Setting up Sublist3r...
+echo [8/10] Setting up Sublist3r...
 cd /d "%TOOLS_DIR%"
 if not exist sublist3r mkdir sublist3r
 cd /d "%TOOLS_DIR%\sublist3r"
 python -m venv sublist3rvenv
 call sublist3rvenv\Scripts\activate.bat
 python -m pip install --upgrade pip
-python -m pip install sublist3r
+python -m pip install sublist3r certifi
 call deactivate
 
 echo.
-echo [9/9] Setting up bot virtual environment...
+echo [9/10] Setting up bot virtual environment...
 cd /d "%BOT_DIR%"
 python -m venv discordbotvenv
 call discordbotvenv\Scripts\activate.bat
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 call deactivate
+
+echo.
+echo [10/10] Installing child-process SSL patch...
+"%BOT_DIR%\discordbotvenv\Scripts\python.exe" "%BOT_DIR%\install_tool_ssl_patch.py"
 
 echo.
 echo ================================================
