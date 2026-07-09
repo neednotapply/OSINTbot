7611650bcaf77209e10f711d9f83e231d15b9934
NeedNotApply <mrnickvoice@gmail.com>
2026-07-08 21:18:40 -0700

    Apply bot parser maintenance during Windows setup

--- setup.bat
+++ setup.bat
@@ -34,7 +34,7 @@ for /f "delims=" %%P in ('python -c "import site; print(site.USER_BASE + r'\Scri
 set "PATH=%USER_PY_SCRIPTS%;%USERPROFILE%\.local\bin;%PATH%"
 
 echo.
-echo [1/12] Setting up Sherlock...
+echo [1/13] Setting up Sherlock...
 cd /d "%TOOLS_DIR%"
 if not exist sherlock mkdir sherlock
 cd /d "%TOOLS_DIR%\sherlock"
@@ -46,7 +46,7 @@ python -m pip install --force-reinstall "%BOT_DIR%\tool_shims"
 call deactivate
 
 echo.
-echo [2/12] Cloning and setting up cupidcr4wl...
+echo [2/13] Cloning and setting up cupidcr4wl...
 cd /d "%TOOLS_DIR%"
 if not exist cupidcr4wl git clone https://github.com/OSINTI4L/cupidcr4wl
 cd /d "%TOOLS_DIR%\cupidcr4wl"
@@ -58,7 +58,7 @@ python -m pip install --upgrade certifi
 call deactivate
 
 echo.
-echo [3/12] Cloning and setting up blackbird...
+echo [3/13] Cloning and setting up blackbird...
 cd /d "%TOOLS_DIR%"
 if not exist blackbird git clone https://github.com/p1ngul1n0/blackbird
 cd /d "%TOOLS_DIR%\blackbird"
@@ -71,7 +71,7 @@ python -m pip install --upgrade certifi
 call deactivate
 
 echo.
-echo [4/12] Setting up holehe...
+echo [4/13] Setting up holehe...
 cd /d "%TOOLS_DIR%"
 if not exist holehe mkdir holehe
 cd /d "%TOOLS_DIR%\holehe"
@@ -83,7 +83,7 @@ python -m pip install --force-reinstall "%BOT_DIR%\tool_shims"
 call deactivate
 
 echo.
-echo [5/12] Setting up user-scanner...
+echo [5/13] Setting up user-scanner...
 cd /d "%TOOLS_DIR%"
 if not exist user-scanner git clone https://github.com/mishakorzik/UserFinder user-scanner
 cd /d "%TOOLS_DIR%\user-scanner"
@@ -95,7 +95,7 @@ python -m pip install --force-reinstall "%BOT_DIR%\tool_shims"
 call deactivate
 
 echo.
-echo [6/12] Setting up whois...
+echo [6/13] Setting up whois...
 cd /d "%TOOLS_DIR%"
 if not exist whois mkdir whois
 cd /d "%TOOLS_DIR%\whois"
@@ -106,7 +106,7 @@ python -m pip install python-whois certifi
 call deactivate
 
 echo.
-echo [7/12] Setting up theHarvester...
+echo [7/13] Setting up theHarvester...
 cd /d "%TOOLS_DIR%"
 if not exist theHarvester mkdir theHarvester
 cd /d "%TOOLS_DIR%\theHarvester"
@@ -117,7 +117,7 @@ python -m pip install theHarvester certifi
 call deactivate
 
 echo.
-echo [8/12] Setting up Sublist3r...
+echo [8/13] Setting up Sublist3r...
 cd /d "%TOOLS_DIR%"
 if not exist sublist3r mkdir sublist3r
 cd /d "%TOOLS_DIR%\sublist3r"
@@ -128,7 +128,7 @@ python -m pip install sublist3r certifi
 call deactivate
 
 echo.
-echo [9/12] Setting up bot virtual environment...
+echo [9/13] Setting up bot virtual environment...
 cd /d "%BOT_DIR%"
 python -m venv discordbotvenv
 call discordbotvenv\Scripts\activate.bat
@@ -137,15 +137,19 @@ python -m pip install -r requirements.txt
 call deactivate
 
 echo.
-echo [10/12] Installing Blackbird wrapper...
+echo [10/13] Applying bot parser maintenance patches...
+"%BOT_DIR%\discordbotvenv\Scripts\python.exe" "%BOT_DIR%\tool_shims\bot_maintenance.py" "%BOT_DIR%"
+
+echo.
+echo [11/13] Installing Blackbird wrapper...
 "%BOT_DIR%\discordbotvenv\Scripts\python.exe" -m osintbot_tool_shims --patch-blackbird "%BOT_DIR%"
 
 echo.
-echo [11/12] Installing child-process SSL patch...
+echo [12/13] Installing child-process SSL patch...
 "%BOT_DIR%\discordbotvenv\Scripts\python.exe" -m osintbot_tool_shims --install-ssl-patch "%BOT_DIR%"
 
 echo.
-echo [12/12] Verifying tool shim entrypoints...
+echo [13/13] Verifying tool shim entrypoints...
 "%TOOLS_DIR%\sherlock\sherlockvenv\Scripts\sherlock.exe" test --timeout 3 >nul 2>nul
 "%TOOLS_DIR%\holehe\holehevenv\Scripts\holehe.exe" test@example.com --timeout 3 >nul 2>nul
 "%TOOLS_DIR%\user-scanner\userscannervenv\Scripts\user-scanner.exe" -u test --timeout 3 >nul 2>nul
